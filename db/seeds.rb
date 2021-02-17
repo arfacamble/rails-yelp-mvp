4424ea04f1419e18ce56a9a096e31d08d059bd85
Arthur Campbell <arfa.camble@gmail.com>
2021-02-17 12:56:59 +0000

    controllers for restaurants and reviews inc views. some styling. index and show pages for restaurant.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'deleting restaurants'
+Restaurant.destroy_all
+puts 'making 10 new restos'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Quotes::Shakespeare.as_you_like_it_quote,
+    address: Faker::Address.full_address,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "saving #{restaurant.name}"
+  restaurant.save
+  rand(1..2).times do
+    review = Review.new(
+      content: Faker::Quotes::Shakespeare.king_richard_iii_quote,
+      rating: rand(0..5)
+    )
+    review.restaurant = restaurant
+    puts 'saving review'
+    review.save
+  end
+end
