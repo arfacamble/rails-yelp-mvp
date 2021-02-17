77465b59519222f62b6e1c84f273d40b034c4ff9
Arthur Campbell <arfa.camble@gmail.com>
2021-02-17 11:52:13 +0000

    restaurant and review models with validations

---
+++ app/models/review.rb
@@ -0,0 +1,10 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, numericality: {
+    only_integer: true,
+    less_than: 6,
+    greater_than_or_equal_to: 0
+  }
+end
