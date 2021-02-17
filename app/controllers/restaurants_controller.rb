9e8948537d86ebdc067bf8f092a6a02147ab594d
Arthur Campbell <arfa.camble@gmail.com>
2021-02-17 14:27:06 +0000

    final methods, integrate add reviews into resto show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,9 +4,26 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new restaurant_params
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,23 @@
 class ReviewsController < ApplicationController
-  def new
+  before_action :set_restaurant
+
+  def create
+    @review = Review.new review_params
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
